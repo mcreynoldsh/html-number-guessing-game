a98b8b8018ec7f4f40264ea2211b270811514519
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-06-15 17:19:24 -0500

    initial

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+let randomNum = Math.floor(Math.random()*101)
+
+const handleFormSubmit = (evt) => {
+    evt.preventDefault() 
+    const formData = new FormData(evt.target);
+    const formProps = Object.fromEntries(formData);
+    showGuess(formData.get("numberGuess"))
+}
+
+const showGuess = (number) => { 
+    console.log(randomNum)
+    let greetingOutput = document.getElementById("output")
+    let newOutput = document.createElement("div")
+    newOutput.innerHTML= number
+    let parentDiv = document.getElementById("parent-div")
+    if(number==randomNum){
+        if (greetingOutput) {
+        greetingOutput.innerHTML = "CORRECT!"
+        }
+    }    
+    else if(number<randomNum){
+        if (greetingOutput) {
+            greetingOutput.innerHTML = "TOO LOW, try again"
+            }
+    }
+    else if(number>randomNum){
+        if (greetingOutput) {
+            greetingOutput.innerHTML = "TOO HIGH, try again"
+            }
+    }
+    if (parentDiv){
+        parentDiv.appendChild(newOutput)
+    }
+  }
+  
